0674f3a92fcf96c76472cff8abb4ffae1a75a88d
Joseph Assiga <jassigaa@redhat.com>
2023-02-15 23:10:12 +0100

    Adding custom metrics

--- Controllers/WelcomeObservabilityController.cs
+++ Controllers/WelcomeObservabilityController.cs
@@ -1,15 +1,27 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Prometheus;
 
 namespace observabilityapp.Controllers;
+
+
 
 [ApiController]
 [Route("welcome")]
 public class WelcomeObservabilityController : ControllerBase
 {
-     [HttpGet(Name = "welcome")]
+    public readonly Counter IterationCount;
+
+    public WelcomeObservabilityController()
+    {
+        IterationCount = Metrics.CreateCounter("ei_totals_call", "Number of iterations that the sample service has ever executed.");
+    }
+    [HttpGet(Name = "welcome")]
     public String Get()
     {
-       return "Welcome to EI Custom Metrics";
+
+        // We finished one iteration of the service's work.
+        IterationCount.Inc();
+        return "Welcome to EI Custom Metrics";
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Define an HTTP client that reports metrics about its usage, to be used by a sample background service.
+//builder.Services.AddHttpClient(SampleService.HttpClientName).UseHttpClientMetrics();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,9 +22,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else{
+else
+{
      app.UseExceptionHandler("/Error");
 }
+
+// Capture metrics about all received HTTP requests.
+app.UseHttpMetrics();
 
 app.UseStaticFiles();
 
@@ -28,5 +39,21 @@ app.MapRazorPages();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    // Enable the /metrics page to export Prometheus metrics.
+    // Open http://localhost:5099/metrics to see the metrics.
+    //
+    // Metrics published in this sample:
+    // * built-in process metrics giving basic information about the .NET runtime (enabled by default)
+    // * metrics from .NET Event Counters (enabled by default, updated every 10 seconds)
+    // * metrics from .NET Meters (enabled by default)
+    // * metrics about requests made by registered HTTP clients used in SampleService (configured above)
+    // * metrics about requests handled by the web app (configured above)
+    // * ASP.NET health check statuses (configured above)
+    // * custom business logic metrics published by the SampleService class
+    endpoints.MapMetrics();
+});
 
 app.Run();
